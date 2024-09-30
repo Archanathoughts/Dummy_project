5ee3a7e0bb6d44800ddfd4d06af9c34f47cb6812
Archana <a.rchana0486@gmail.com>
2024-09-30 16:23:24 +0530

    new

---
+++ githubpractice2/src/test/java/dummy/Sample1.java
@@ -0,0 +1,9 @@
+package dummy;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		System.out.println("dummy");
+		System.out.println("dummy");
+		System.out.println("dummy");
+	}
+  }
