b803c93f2b31817e25f422466425e1344e9bae96
Archana <a.rchana0486@gmail.com>
2024-09-30 16:43:19 +0530

    from sam

--- githubpractice2/src/test/java/dummy/NewTest.java
+++ githubpractice2/src/test/java/dummy/NewTest.java
@@ -4,6 +4,8 @@ public class NewTest {
 	public static void main(String[] args) {
 		System.out.println("dum");
 		System.out.println("dum");
+		System.out.println("dum2");
+		System.out.println("dum2");
 		
 	}
 
